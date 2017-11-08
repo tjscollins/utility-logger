dd1cc105207c6a795506f856b12422872fd18968
Tyler Collins <tjscollins@gmail.com>
2017-11-08 05:37:07 +0000

    jsDoc for enums

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,24 @@
+/**
+ * Enum for logging levels
+ *
+ * @readonly
+ * @enum {number}
+ */
 export const LEVEL = Object.freeze({
-    quiet: 0,
-    error: 1,
-    info: 2,
-    debug: 3,
-    log: 4,
+  quiet: 0,
+  error: 1,
+  info: 2,
+  debug: 3,
+  log: 4,
 });
 
+/**
+ * Enum for logging output modes
+ *
+ * @readonly
+ * @enum {number}
+ */
 export const MODE = Object.freeze({
-    console: 0,
-    file: 1,
+  console: 0,
+  file: 1,
 });
