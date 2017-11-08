56c696e65a9e2747814638ea9fd5a1b574f3854d
Tyler Collins <tjscollins@gmail.com>
2017-11-08 23:45:54 +0000

    Build chain improvements

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,25 @@
 // rollup.config.js
 import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
+import builtins from 'rollup-plugin-node-builtins';
+// import globals from 'rollup-plugin-node-globals';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import { minify } from 'uglify-es';
 
 export default {
   // core input options
   input: 'src/index.js', // required
-  // external,
+  // external: ['colors'],
   plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
+    builtins(),
+    // globals(),
     babel(),
     uglify({}, minify),
   ],
@@ -26,7 +38,9 @@ export default {
     file: 'dist/utility-logger.js', // required
     format: 'umd', // required
     name: 'UtilityLogger',
-    // globals,
+    globals: {
+      // colors: 'colors',
+    },
 
     // advanced output options
     // paths,
