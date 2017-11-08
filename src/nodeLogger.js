373b02ebd79b0a4dd3237a81bed9f47d36b26a95
Tyler Collins <tjscollins@gmail.com>
2017-11-08 06:16:23 +0000

    Make NodeLogger.fileFormat into private method like BaseLogger.consoleFormat

--- src/nodeLogger.js
+++ src/nodeLogger.js
@@ -1,24 +1,88 @@
 import fs from 'fs';
+import path from 'path';
 
 import BaseLogger from './BaseLogger';
-import { LEVEL, MODE } from './constants';
+import { LEVEL, MODE, DEFAULT_NODE_OPTS } from './constants';
 
-export default class nodeLogger extends baseLogger {
-  constructor(opts) {
-    super(opts.level);
+export default class nodeLogger extends BaseLogger {
+  constructor(suppliedOptions) {
+    const options = Object.assign({}, DEFAULT_NODE_OPTS, suppliedOptions);
+    super(options);
 
-    if (Object.keys(MODE).indexOf(opts.mode) < 0) {
-      throw new TypeError(`Invalid log mode ${opts.mode}`);
+    if (options.mode !== undefined && Object.keys(MODE).indexOf(options.mode) < 0) {
+      throw new TypeError(`Invalid log mode ${options.mode}`);
     }
 
-    this.mode = MODE[opts.mode];
+    this.mode = MODE[options.mode];
 
-    if (this.mode == MODE.file) {
-      this.logFile = opts.logFile;
+    if (this.mode === MODE.file) {
+      this.logFile = options.logFile || `${path.basename(process.argv[1])}.log`;
     }
   }
 
-  fileFormat(data, level) {
+  log(...args) {
+    if (this.level >= LEVEL.log) {
+      switch (this.mode) {
+        case MODE.console:
+          super.log(...args);
+          break;
+        case MODE.file:
+          fs.appendFile(this.logFile, this._fileFormat(args, LEVEL.log));
+          break;
+        default:
+          throw new TypeError(`Invalid log mode ${this.mode}`);
+      }
+    }
+  }
+
+  error(...args) {
+    if (this.level >= LEVEL.error) {
+      switch (this.mode) {
+        case MODE.console:
+          super.error(...args);
+          break;
+        case MODE.file:
+          fs.appendFile(this.logFile, this._fileFormat(args, LEVEL.error));
+          break;
+        default:
+          throw new TypeError(`Invalid log mode ${this.mode}`);
+      }
+    }
+  }
+
+  info(...args) {
+    if (this.level >= LEVEL.info) {
+      switch (this.mode) {
+        case MODE.console:
+          super.info(...args);
+          break;
+        case MODE.file:
+          fs.appendFile(this.logFile, this._fileFormat(args, LEVEL.info));
+          break;
+        default:
+          throw new TypeError(`Invalid log mode ${this.mode}`);
+      }
+    }
+  }
+
+  debug(...args) {
+    if (this.level >= LEVEL.debug) {
+      switch (this.mode) {
+        case MODE.console:
+          super.debug(...args);
+          break;
+        case MODE.file:
+          fs.appendFile(this.logFile, this._fileFormat(args, LEVEL.debug));
+          break;
+        default:
+          throw new TypeError(`Invalid log mode ${this.mode}`);
+      }
+    }
+  }
+
+  /* --------Private Methods-------- */
+
+  _fileFormat(data, level) {
     const timestamp = Date();
     let logString;
 
@@ -41,57 +105,5 @@ export default class nodeLogger extends baseLogger {
 
     logString += data.reduce(this._stringify, '');
     return logString;
-  }
-
-  log(...args) {
-    switch (this.mode) {
-      case MODE.console:
-        super.log(...args);
-        break;
-      case MODE.file:
-        fs.appendFile(this.logFile, this.fileFormat(args, LEVEL.log));
-        break;
-      default:
-        throw new TypeError(`Invalid log mode ${this.mode}`);
-    }
-  }
-
-  error(...args) {
-    switch (this.mode) {
-      case MODE.console:
-        super.error(...args);
-        break;
-      case MODE.file:
-        fs.appendFile(this.logFile, this.fileFormat(args, LEVEL.error));
-        break;
-      default:
-        throw new TypeError(`Invalid log mode ${this.mode}`);
-    }
-  }
-
-  info(...args) {
-    switch (this.mode) {
-      case MODE.console:
-        super.info(...args);
-        break;
-      case MODE.file:
-        fs.appendFile(this.logFile, this.fileFormat(args, LEVEL.info));
-        break;
-      default:
-        throw new TypeError(`Invalid log mode ${this.mode}`);
-    }
-  }
-
-  debug(...args) {
-    switch (this.mode) {
-      case MODE.console:
-        super.debug(...args);
-        break;
-      case MODE.file:
-        fs.appendFile(this.logFile, this.fileFormat(args, LEVEL.debug));
-        break;
-      default:
-        throw new TypeError(`Invalid log mode ${this.mode}`);
-    }
   }
 }
