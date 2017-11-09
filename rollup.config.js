55db298050756cb54ce21f677362676229ed6c1d
Tyler Collins <tjscollins@gmail.com>
2017-11-09 02:19:47 +0000

    Make build work for browser

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@
 import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
 import builtins from 'rollup-plugin-node-builtins';
-// import globals from 'rollup-plugin-node-globals';
+import globals from 'rollup-plugin-node-globals';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import { minify } from 'uglify-es';
@@ -15,11 +15,11 @@ export default {
     resolve({
       jsnext: true,
       main: true,
-      browser: true,
+      // browser: true,
     }),
     commonjs(),
     builtins(),
-    // globals(),
+    globals(),
     babel(),
     uglify({}, minify),
   ],
