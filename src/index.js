f49996db7e45c19e6c86a3bd698d9bfc1b92d0b0
Tyler Collins <tjscollins@gmail.com>
2017-11-09 02:20:45 +0000

    Change selection order to get Browser vs Node to work properly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import NodeLogger from './NodeLogger';
 import BrowserLogger from './BrowserLogger';
 
-const Logger = (typeof process !== 'undefined') && (process.release.name === 'node') ? NodeLogger : BrowserLogger;
-
-export default Logger;
+export default (function selectLogger() {
+  if (typeof window !== 'undefined') {
+    return BrowserLogger;
+  } else if (typeof global !== 'undefined') {
+    return NodeLogger;
+  }
+  throw new Error('Unknown environment');
+}());
