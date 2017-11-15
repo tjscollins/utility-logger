0c808d93f1566a92cd3314008ca2615f6226280f
Tyler Collins <tjscollins@gmail.com>
2017-11-15 05:13:55 +0000

    Added file mode test to demo

--- __tests__/demo.js
+++ __tests__/demo.js
@@ -6,3 +6,10 @@ log.log('Basic forced log statement');
 log.info('Informative content');
 log.error('Oh no, something went horribly wrong.');
 log.debug('Here\'s what I know about it...');
+
+const file = new Logger({ level: 'debug', mode: 'file' });
+
+file.log('Basic forced log statement');
+file.info('Informative content');
+file.error('Oh no, something went horribly wrong.');
+file.debug('Here\'s what I know about it...');
